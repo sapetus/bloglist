94ab1b497a50b2566e6b38a5d7e35816001b7cd2
Sampo Pitkänen <sampopitkanen@hotmail.com>
2021-11-09 14:26:40 +0200

    added port

--- bloglist-backend/utils/config.js
+++ bloglist-backend/utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 const MONGODB_URI = process.env.NODE_ENV === 'test'
     ? process.env.TEST_MONGODB_URI
